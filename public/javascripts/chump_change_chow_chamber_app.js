6bbf5ca16dd8be0510e49a2672246a82f49be8e5
Charles C. Williams, Jr <chuckwilliams37@gmail.com>
2014-12-03 15:23:51 +0000

    setting up yummly API

--- public/javascripts/chump_change_chow_chamber_app.js
+++ public/javascripts/chump_change_chow_chamber_app.js
@@ -1,8 +1,18 @@
 //ChumpChangeChowChamberApp.js
 
 angular.module('ChumpChangeChowChamberApp', ['ui.bootstrap','restangular','ngRoute', 'ngAnimate'])
-.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
+.config(['$routeProvider', 'restangular',
+  function($routeProvider, RestangularProvider) {
+    var yummly = {
+      appId : a86b6d59,
+      key : a7c56372c0412e6698ec5d34d33f9101,
+      baseUrl: "https://ismaelc-yummly1.p.mashape.com/api"
+    };
+    
+    RestangularProvider.setBaseUrl(yummly.baseUrl);
+    RestangularProvider.setDefaultRequestParams({_app_id: yummly.appId , _app_key: yummly.key });
+    
+    
     $routeProvider
       .when('/whats_in_the_pantry', {
         templateUrl: '#/whats_in_the_pantry.html/',
